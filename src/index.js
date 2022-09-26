69708d0c60b206edab9937eddd5b4f83faaaf27f
keshviiipatel <keshviiipatel@gmail.com>
2022-09-26 18:56:12 +0530

    react _node

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function MyApi(){
+  const [data,setData]=useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:3003/faculties')
+    .then((res)=>res.json())
+    .then((res)=>{setData(res)})
+  },[])
+  
+  const formetedfaculty = data.map((fac)=>{
+
+    return (
+      <>
+         <h2>FacultyId:{fac.FacultyId}</h2>,
+         <h2>FacultyName:{fac.FacultyName}</h2>,
+         <h2>FacultyAge:{fac.FacultyAge}</h2>
+         </>
+         )
+  });
+
+  return formetedfaculty;
+
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <MyApi/>
 );
 
 // If you want to start measuring performance in your app, pass a function
